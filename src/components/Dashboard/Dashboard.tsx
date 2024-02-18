a84a3e2490780b684c1dad34a0a6f183e56e10b0
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-18 11:13:36 -0300

    fix: responsive height

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -10,7 +10,7 @@ export const Dashboard: FunctionComponent<DashboardProps> = ({
 	currentBoard,
 }) => {
 	return (
-		<main className='flex gap-16 min-w-fit w-full pl-[60px] h-full p-8 pt-[100px] bg-background-color text-white'>
+		<main className='flex gap-16 min-w-fit w-full pl-[60px] min-h-full h-auto p-8 pt-[100px] bg-background-color text-white'>
 			{currentBoard?.columns.map((column) => {
 				return <DashboardColumn key={column.id} column={column} />;
 			})}
