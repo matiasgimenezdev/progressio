ae64da66a39a68cca342a55b0164dfc937fe3f8a
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-18 11:14:52 -0300

    feat: add focus lock & remove scroll when menu is open

--- src/components/BoardSelector/BoardSelector.tsx
+++ src/components/BoardSelector/BoardSelector.tsx
@@ -1,8 +1,10 @@
 import { FunctionComponent, useState } from 'react';
 import { Board } from '../../types';
-import { CaretDoubleRight, Cube, SignOut, Plus } from '@phosphor-icons/react';
-import { CreateBoardForm } from '../CreateBoardForm/CreateBoardForm';
 import { useKeydown } from '../../hooks';
+import { CreateBoardForm } from '../CreateBoardForm/CreateBoardForm';
+import { CaretDoubleRight, Cube, SignOut, Plus } from '@phosphor-icons/react';
+import FocusLock from 'react-focus-lock';
+import { RemoveScroll } from 'react-remove-scroll';
 
 type BoardSelectorProps = {
 	boards: Board[];
@@ -32,107 +34,113 @@ export const BoardSelector: FunctionComponent<BoardSelectorProps> = ({
 	}
 
 	return (
-		<>
-			<aside
-				className={`absolute h-[calc(100%-80px)] ${
-					isOpen
-						? 'w-[250px] animate-openmenu'
-						: 'w-[40px] animate-closemenu'
-				} bg-primary-color left-0 top-[80px] text-white`}
-			>
-				<nav
-					className={`w-full h-full relative ${
-						isOpen ? 'text-right' : 'text-center'
-					} `}
+		<RemoveScroll enabled={isOpen}>
+			<FocusLock disabled={!isOpen}>
+				<aside
+					className={`absolute h-[calc(100%-80px)] ${
+						isOpen
+							? 'w-[250px] max-w-[250px]'
+							: 'w-[40px] max-w-[40px]'
+					} bg-primary-color left-0 top-[80px] text-white transition-all duration-500`}
 				>
-					<button
-						className={`h-10 w-10 mt-4 cursor-pointer ${
-							isOpen && 'rotate-180 mr-3 right-0'
+					<nav
+						className={`w-full h-full relative ${
+							isOpen ? 'text-right' : 'text-center'
 						} `}
-						onClick={() => {
-							setIsOpen(!isOpen);
-							setShowModal(false);
-						}}
 					>
-						<CaretDoubleRight
-							size={14}
-							weight='bold'
-							className='ml-auto mr-auto'
-						/>
-					</button>
-					<ul
-						className={`transition-transform mt-2 h-auto ${
-							isOpen
-								? 'w-[230px] opacity-1 animate-openmenuitems'
-								: 'w-0 opacity-0 animate-closemenuitems'
-						}`}
-					>
-						{boards.length > 0 ? (
-							boards.map((board) => {
-								const { name, id } = board;
-								return (
-									<li
-										className={`text-left w-[190px] mt-2 h-[60px] flex items-center gap-2 pl-1.5 ${
-											currentBoardId === id
-												? 'bg-secondary-color rounded-r-full'
-												: 'hover:opacity-80 hover:bg-secondary-color rounded-r-full cursor-pointer'
-										} ${!isOpen && 'hidden'}`}
-										key={id}
-										onClick={() =>
-											handleBoardItemClick(board)
-										}
-										onKeyDown={(event) =>
-											event.key === 'Enter' &&
-											handleBoardItemClick(board)
-										}
-										tabIndex={0}
-									>
-										<Cube size={22} weight='light' />
-										{name}
-									</li>
-								);
-							})
-						) : (
-							<li className='text-center font-medium'>
-								No boards yet
-							</li>
-						)}
-					</ul>
-					<button
-						className={`text-center min-w-[160px] opacity-1 ml-auto mr-auto block text-sm font-medium rounded-lg bg-secondary-color p-2 px-6 pr-5 mt-6 w-fit ${
-							!isOpen && 'hidden opacity-0'
-						} hover:brightness-90 group animate-opacity`}
-						onClick={() => {
-							setShowModal(true);
-							setIsOpen(false);
-						}}
-					>
-						New board{' '}
-						<Plus
-							size={14}
-							weight='bold'
-							className='inline-block ml-1 relative group:hover:cursor-pointer'
-						/>
-					</button>
-					<button
-						className={`absolute bottom-8 left-4 group ${
-							!isOpen && 'hidden'
-						}`}
-					>
-						Logout{' '}
-						<SignOut
-							size={28}
-							className='inline-block ml-1 relative bottom-0.5 group-hover:fill-red-600'
-						/>
-					</button>
-				</nav>
-			</aside>
-			<CreateBoardForm
-				showModal={showModal}
-				closeModal={closeModal}
-				handleBoardItemClick={handleBoardItemClick}
-				handleCreateBoard={handleCreateBoard}
-			/>
-		</>
+						<button
+							className={`h-10 w-10 mt-4 cursor-pointer ${
+								isOpen && 'rotate-180 mr-3 right-0'
+							} `}
+							onClick={() => {
+								setIsOpen(!isOpen);
+								setShowModal(false);
+							}}
+						>
+							<CaretDoubleRight
+								size={14}
+								weight='bold'
+								className='ml-auto mr-auto'
+							/>
+						</button>
+						<ul
+							className={`transition-transform mt-2 h-auto ${
+								isOpen
+									? 'w-[230px] opacity-1 animate-openmenuitems'
+									: 'w-0 opacity-0 animate-closemenuitems'
+							}`}
+						>
+							{boards.length > 0 ? (
+								boards.map((board) => {
+									const { name, id } = board;
+									return (
+										<li
+											className={`text-left w-[190px] mt-2 h-[60px] flex items-center gap-2 pl-1.5 ${
+												currentBoardId === id
+													? 'bg-secondary-color rounded-r-full'
+													: 'hover:opacity-80 hover:bg-secondary-color rounded-r-full cursor-pointer'
+											} ${
+												!isOpen &&
+												'translate-x-[-100%] opacity-0'
+											}`}
+											key={id}
+											onClick={() =>
+												handleBoardItemClick(board)
+											}
+											onKeyDown={(event) =>
+												event.key === 'Enter' &&
+												handleBoardItemClick(board)
+											}
+											tabIndex={0}
+										>
+											<Cube size={22} weight='light' />
+											{name}
+										</li>
+									);
+								})
+							) : (
+								<li className='text-center font-medium'>
+									No boards yet
+								</li>
+							)}
+						</ul>
+						<button
+							className={`text-center min-w-[160px] opacity-1 ml-auto mr-auto  bg-secondary-color p-2 px-6 pr-5 mt-6 w-fit group transition-opacity duration-300 delay-150 ${
+								!isOpen &&
+								'translate-x-[-100%] text-transparent bg-transparent opacity-0'
+							} block text-sm font-medium rounded-lg hover:brightness-90`}
+							onClick={() => {
+								setShowModal(true);
+								setIsOpen(false);
+							}}
+						>
+							New board{' '}
+							<Plus
+								size={14}
+								weight='bold'
+								className='inline-block ml-1 relative group:hover:cursor-pointer'
+							/>
+						</button>
+						<button
+							className={`absolute bottom-8 left-4 group ${
+								!isOpen && 'hidden'
+							}`}
+						>
+							Logout{' '}
+							<SignOut
+								size={28}
+								className='inline-block ml-1 relative bottom-0.5 group-hover:fill-red-600'
+							/>
+						</button>
+					</nav>
+				</aside>
+				<CreateBoardForm
+					showModal={showModal}
+					closeModal={closeModal}
+					handleBoardItemClick={handleBoardItemClick}
+					handleCreateBoard={handleCreateBoard}
+				/>
+			</FocusLock>
+		</RemoveScroll>
 	);
 };
