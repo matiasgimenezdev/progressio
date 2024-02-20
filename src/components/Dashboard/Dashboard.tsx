7cdbdb6186282d75a8eff8f419432e2811e9e44d
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-20 09:55:07 -0300

    feat: update add column button styles

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -60,7 +60,7 @@ export const Dashboard: FunctionComponent<DashboardProps> = ({
 					);
 				})}
 			<button
-				className={`mt-3 min-w-[132px] h-fit bg-secondary-color rounded-lg text-sm p-3 px-5 cursor-pointer hover:brightness-90`}
+				className={`mt-3 min-w-[132px] h-fit border-2 border-white bg-transparent text-white rounded-lg text-sm p-3 px-5 cursor-pointer hover:brightness-90`}
 				onClick={() => showModal()}
 			>
 				+ Add column
